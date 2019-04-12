2897d18a4c5c2bbccd412ee4231a1a0420523093
keiko <nknknk031@gmail.com>
2019-04-12 13:52:31 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  include SessionsHelper
+  
+  private
+  
+  def require_user_logged_in
+      # unless は if の反対で、if は true のときに実行するが unless は false のときに実行する
+      unless logged_in?
+      redirect_to login_url
+      end
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+    before_save { self.email.downcase! }
+    # User保存の前に{}ないを実行する
+    # self.email.downcase! 文字を全て小文字に変換する
+    # ! をつけて自分自身を直接変換する
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :email, presence: true, length: {maximum: 255 },
+                        format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                        uniqueness: { case_sensitive: false }
+                        # uniqueness は重複を許さないバリデーション
+                        # case_sensitive: falseは大文字と小文字を区別しない
+    has_secure_password
+end
